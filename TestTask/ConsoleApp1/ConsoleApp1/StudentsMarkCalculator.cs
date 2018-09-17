28d45e8e6446a10bccfac36956d121e21d6f5ad3
eartimenya <eartimenya@pangramia.com>
2018-09-17 22:32:49 +0300

    Test task

---
+++ TestTask/ConsoleApp1/ConsoleApp1/StudentsMarkCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    static class StudentsMarkCalculator
+    {
+
+        public static double GetAverageMarkByStudents(List<Student> students)
+        {
+            double avgSum = 0;
+            foreach (var student in students)
+            {
+                avgSum += student.GetAverageMark();
+            }
+            return avgSum / students.Count();
+        }
+
+    }
+}
